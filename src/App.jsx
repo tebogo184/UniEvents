7d78cdc3779b1f60f7888b6fc7afe9571fe57f81
tebogo184 <tebogotshukudu184@gmail.com>
2026-01-14 04:06:18 +0200

    Setting up the project

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import{ BrowserRouter,Routes,Route} from 'react-router-dom'
+import Home from './Pages/Home'
+
+function App() {
+
+
+  return (
+   <BrowserRouter>
+   <Routes>
+    <Route path='/' element={<Home/>}/>
+   </Routes>
+   </BrowserRouter>
+  )
+}
+
+export default App
